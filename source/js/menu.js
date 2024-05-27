5225bc9b6e90ed8f0c9ea922365a8453c51809a0
ShkurinskayaIrina <shkurinskayaIrina@gmail.com>
2024-05-27 09:53:14 +0300

    Поправлено открытие навигации сайта

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,4 +1,4 @@
-const navSiteMenu = document.querySelector('.page-header__nav--nojs');
+const navSiteMenu = document.querySelector('.page-header__nav');
 const menuToggler = navSiteMenu.querySelector('.page-header__toggler');
 
 navSiteMenu.classList.remove('page-header__nav--nojs');
@@ -8,12 +8,12 @@ export const handleClickMenuToggler = () => {
   navSiteMenu.classList.toggle('page-header__nav--open');
 };
 
-if (window.matchMedia('(max-width: 320px)').matches) {
+if (window.matchMedia('(max-width: 767px)').matches) {
   menuToggler.addEventListener('click', handleClickMenuToggler);
 }
 
 window.addEventListener('resize', () => {
-  if (window.matchMedia('(max-width: 320px)').matches) {
+  if (window.matchMedia('(max-width: 767px)').matches) {
     menuToggler.addEventListener('click', handleClickMenuToggler);
   } else {
     menuToggler.removeEventListener('click', handleClickMenuToggler);
